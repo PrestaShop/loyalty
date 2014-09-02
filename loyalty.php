28cae7bcbd76c63ea07e9716303b7df932d6749a
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2014-09-02 17:34:13 +0200

    // Version updated to v1.2.3

--- loyalty.php
+++ loyalty.php
@@ -42,7 +42,7 @@ class Loyalty extends Module
 	{
 		$this->name = 'loyalty';
 		$this->tab = 'pricing_promotion';
-		$this->version = '1.2.2';
+		$this->version = '1.2.3';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
